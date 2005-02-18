e6f5946ecc7d9184f82f462ff2eabb7d2daae541
pez <pez>
2005-02-18 06:00:52 +0000

    Vars we're going to use.

--- libxmms_tracking.c
+++ libxmms_tracking.c
@@ -1,4 +1,4 @@
-/* $Id: libxmms_tracking.c,v 1.4 2005/02/18 05:46:19 pez Exp $ */
+/* $Id: libxmms_tracking.c,v 1.5 2005/02/18 06:00:52 pez Exp $ */
 /* Some Includes */
 #include <pthread.h>
 #include <unistd.h>
@@ -22,9 +22,21 @@
 /* Local includes */
 #include "config.h"
 
+/* Other defines */
+#define CFGCAT "xmms_tracking"
+
+/* Hooks */
 static void init(void);
 static void cleanup(void);
 static void configure(void);
+
+/* Other functions */
+static void read_config(void);
+
+/* Static Vars */
+static gint percent_done;
+static gint seconds_past;
+static gchar *cmd_line = NULL;
 
 static GeneralPlugin xmms_tracking =
 {
@@ -46,6 +58,8 @@ GeneralPlugin *get_gplugin_info(void)
 
 static void init(void)
 {
+	read_config();
+
 	fprintf(stderr, "Plugin init\n");
 }
 
@@ -57,4 +71,20 @@ static void cleanup(void)
 static void configure(void)
 {
 	fprintf(stderr, "In configure\n");
+}
+
+static void read_config(void)
+{
+	ConfigFile *cfgfile;
+
+	g_free(cmd_line);
+	cmd_line = NULL;
+
+	if ((cfgfile = xmms_cfg_open_default_file()) != NULL)
+	{
+		xmms_cfg_read_int(cfgfile, CFGCAT, "percent_done", &percent_done);
+		xmms_cfg_read_int(cfgfile, CFGCAT, "seconds_past", &seconds_past);
+		xmms_cfg_read_string(cfgfile, CFGCAT, "cmd_line", &cmd_line);
+		xmms_cfg_free(cfgfile);
+	}
 }

aedaa8ace7f5a94d2402fa3772da51d94e0ce0b3
pez <pez>
2005-02-18 04:33:21 +0000

    initial import

---
+++ libxmms_tracking.c
@@ -0,0 +1,45 @@
+/* Some Includes */
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Some More Includes */
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* GTK Includes */
+#include <gtk/gtk.h>
+
+/* XMMS Includes */
+#include <xmms/plugin.h>
+#include <xmms/configfile.h>
+#include <xmms/xmmsctrl.h>
+#include <xmms/formatter.h>
+
+static void init(void);
+static void cleanup(void);
+
+static GeneralPlugin xmms_tracking =
+{
+	NULL,		/* handle */
+	NULL,		/* filename */
+	-1,		/* xmms_session */
+	NULL,		/* description */
+	init,
+	NULL,
+	NULL,		/* configure_dialog */
+	cleanup,
+};
+
+static void init(void)
+{
+	fprintf(stderr, "Plugin init\n");
+}
+
+static void cleanup(void)
+{
+	fprintf(stderr, "In cleanup\n");
+	/* Not much */
+}
